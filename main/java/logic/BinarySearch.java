e1cb7a9e417c7d1b029a916db3d900a4a00e2be0
komal.bhatt <komal18@navgurukul.org>
2019-06-10 17:00:19 +0530

    Add.TestCases for BinarySearch.

--- main/java/logic/BinarySearch.java
+++ main/java/logic/BinarySearch.java
@@ -6,12 +6,11 @@ public class BinarySearch {
     public static <T extends Comparable<T>> int binarySearch(T element, List<T> elements) {
         int lastIndex = elements.size()-1;
         int firstIndex = 0;
-        while (firstIndex < lastIndex || lastIndex == 0) {
+        while (firstIndex <= lastIndex || lastIndex == 0) {
             int middleIndex = getMiddleIndex(lastIndex, firstIndex);
             if (element.compareTo(elements.get(middleIndex))==0) {
                 return middleIndex;
             } else if (element.compareTo(elements.get(middleIndex)) < 0) {
-                System.out.println();
                 lastIndex = middleIndex-1;
             }  else{
                 firstIndex = middleIndex+1;
--- test/java/BinarySearch/BinarySearchTest.java
+++ test/java/BinarySearch/BinarySearchTest.java
@@ -14,6 +14,9 @@ public class BinarySearchTest {
         assertEquals(0,BinarySearch.binarySearch(10,Arrays.asList(10,19,21,35,78)));
         assertEquals(3,BinarySearch.binarySearch(45,Arrays.asList(10,19,21,45,78)));
         assertEquals(1,BinarySearch.binarySearch(2,Arrays.asList(1,2,180,1000)));
+        assertEquals(1,BinarySearch.binarySearch(2.0,Arrays.asList(1.0,2.0,180.0,1000.0)));
+        assertEquals(4,BinarySearch.binarySearch("e",Arrays.asList("a","b","c","d","e")));
+        assertEquals(3,BinarySearch.binarySearch("hi",Arrays.asList("at", "from", "hello", "hi", "there", "this")));
     }
 
 
